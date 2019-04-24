81b56c268bfccf5ac48c25958e68cf5877b54343
Devin Powell <dpowel11@kent.edu>
2019-04-24 16:19:52 -0600

    small change to make it functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,37 +21,37 @@ var database = firebase.database();
 
 // Function to start game once players have joined - generate buttons
 // function gameStart() {
-    if (playerOne !== '' && playerTwo !== '') {
+    // if (playerOne !== '' && playerTwo !== '') {
         // Create a button for 'Rock'
         var rockButton = $('<button>');
-        // Adding class
-        rockButton.addClass('btn-primary');
-        // Add value to button
-        rockButton.attr('name','Rock');
-        // Add text to Rock Button
-        rockButton.text('Rock'); 
+            // Adding class
+            rockButton.addClass('btn-primary');
+            // Add value to button
+            rockButton.attr('name','Rock');
+            // Add text to Rock Button
+            rockButton.text('Rock'); 
         // Create a button for 'Paper'
         var paperButton = $('<button>');
-        // Adding class
-        paperButton.addClass('btn-primary');
-        // Add value to button
-        paperButton.attr('name','Paper');
-        // Add text
-        paperButton.text('Paper');
+            // Adding class
+            paperButton.addClass('btn-primary');
+            // Add value to button
+            paperButton.attr('name','Paper');
+            // Add text
+            paperButton.text('Paper');
         // Create a button for 'Scissors'
         var scissorsButton = $('<button>');
-        // Add class
-        scissorsButton.addClass('btn-primary');
-        // Add value to button
-        scissorsButton.attr('name','Scissors');
-        // Add Text
-        scissorsButton.text('Scissors');
+            // Add class
+            scissorsButton.addClass('btn-primary');
+            // Add value to button
+            scissorsButton.attr('name','Scissors');
+            // Add Text
+            scissorsButton.text('Scissors');
 
         // Append to HTML
         $('#buttons').append(rockButton);  
         $('#buttons').append(paperButton);        
         $('#buttons').append(scissorsButton);              
-    }
+    // }
 
 // Connect to Game
 
@@ -61,19 +61,14 @@ function checkRound() {
     playerOneChoice === 'Paper' && playerTwoChoice === 'Rock' || 
     playerOneChoice === 'Scissors' && playerTwoChoice === 'Paper') {
         // Add a Win to Player One
-        playeroneWins++;
+        playerOneWins++;
         // Update HTML Win Counter
         $('#player-one-wins').html(playerOneWins);
-
-        // Reset choices
     } else if (playerOneChoice === playerTwoChoice) {
         ties++;
         // Append Tie count to HTML
         $('#ties').html(ties);
-        // Reset Choices
-    } else if (playerOneChoice === 'Scissors' && playerTwoChoice === 'Rock' || 
-    playerOneChoice === 'Rock' && playerTwoChoice === 'Paper' || 
-    playerOneChoice === 'Paper' && playerTwoChoice === 'Scissors') {
+    } else {
         // Add a win to Player Two
         playerTwoWins++;
         // Append Wins to HTML
@@ -90,17 +85,21 @@ function checkRound() {
 
 // Player selecting a choice
 $('body').on('click','.btn-primary', function() {
-    // If player one has not chosen yet
+    // If player one has not yet chosen
     if (playerOneChoice === '') {
         $('#player-one-choice').text($(this).attr('name'));
         playerOneChoice = ($(this).attr('name'));
+        // If player one has chosen and player two has not yet chosen
     } else if (playerOneChoice !== '' && playerTwoChoice === '') {
         $('#player-two-choice').text($(this).attr('name'));
         playerTwoChoice = $(this).attr('name');
         checkRound();
+        // One they have both picked
     } else {
-        // Check Game Logic
+        // checkRound function
         checkRound();
     }
+    console.log('P1 Choice: ', playerOneChoice);
+    console.log('P2 Choice: ', playerTwoChoice);
 })
 
